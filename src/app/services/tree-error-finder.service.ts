e01650803d6c812d112084a7a0c81fa293efddfe
gerard.esteve <gerard.esteve@accenture.com>
2024-01-26 19:43:17 +0100

    common errors check

--- src/app/services/tree-error-finder.service.ts
+++ src/app/services/tree-error-finder.service.ts
@@ -13,11 +13,38 @@ export class TreeErrorFinderService {
   In this code, errorsSubject is a private Subject, so other parts of your code can't push values to it. errors$ is a public Observable that other parts of your code can subscribe to.
   By doing this, you ensure that only the TreeErrorFinderService can control when new values are emitted. This makes your code more predictable and easier to debug, because you know that new values can only be emitted from inside the TreeErrorFinderService. */
 
+  private errorList: any = []
+
   checkErrors(tree: any) {
-    console.log('check errors on the tree')
-    // La cosa que retorna una serie d'errors en forma d'array d'objectes per poder saber en quin node estan i info varia
-    // Quan s'ha fet la comprovació això ha d'avisar a penya. Per exemple al component de tree-error-notifier.
-    // Aquet tindrà importat el servei, pero no sé com fer que es suscrigui a algo d'aquet servei
-    this.errorsSubject.next(['errors'])
+    console.log('checking errors on the tree')
+    for (let node of tree.nodes) {
+      if (this.hasEmptyText(node))
+        this.errorList.push({ type: 'node-empty_text', node: node.id })
+      if (!node.answers) {
+        this.errorList.push({ type: 'node-no_answers', node: node.id })
+        return
+      }
+      for (let answer of node.answers) {
+        if (!answer.join || answer.join.length === 0) {
+          const answerHaveWinOrEnd = answer.events?.find((event: any) => {
+            return event.action === 'win' || event.action === 'end'
+          })
+          if (!answerHaveWinOrEnd || !answer.events) {
+            this.errorList.push({
+              type: 'answer-no_join_nor_end',
+              answer: answer.id,
+            })
+          }
+        }
+        if (this.hasEmptyText(answer))
+          this.errorList.push({ type: 'answer-empty_text', answer: answer.id })
+      }
+    }
+
+    this.errorsSubject.next(this.errorList)
+  }
+
+  hasEmptyText(element: any) {
+    return !element.text || element.text.trim().length === 0
   }
 }
