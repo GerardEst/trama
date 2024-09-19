500de0d8a031f8a44459df40261fa3bf778c4fa5
gerard.esteve <gerard.esteve@accenture.com>
2024-09-19 17:15:11 +0200

    stop tests when push

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env['CI']
-      ? 'https://textandplayasdasd.com'
+      ? 'https://textandplay.com'
       : 'http://localhost:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
