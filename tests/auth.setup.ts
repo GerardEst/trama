586ca80615bb37e6953d4924e96adcf16a72231d
gerard.esteve <gerard.esteve@accenture.com>
2024-09-19 16:46:35 +0200

    use env

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,15 +1,16 @@
 import { test as setup, expect } from '@playwright/test'
+import 'dotenv/config'
 
 const authFile = 'playwright/.auth/user.json'
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
   await page.goto('/login')
-  await page.getByLabel('Your email').fill('gesteve.12@gmail.com')
-  await page.getByLabel('Your password').fill('123456')
+  await page.getByLabel('Your email').fill(process.env['USERNAME'] || '')
+  await page.getByLabel('Your password').fill(process.env['PASSWORD'] || '')
   await page.getByRole('button', { name: 'Login' }).click()
 
-  await page.waitForURL('http://localhost:4200/dashboard')
+  await page.waitForURL('/dashboard')
 
   await expect(page.getByText('Gerard')).toBeVisible()
 
