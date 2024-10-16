f308abd5a1bfb53c70da3dc0ea9c128754be2ff8
gerard.esteve <gesteve.12@gmail.com>
2024-10-16 09:57:19 +0200

    better pricing toggle

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,6 +5,6 @@ export const PRICING = {
   CREATOR_YEARLY_LINK: 'https://buy.stripe.com/14kaEZ8xK0jK4Fi28d',
   PRO_MONTHLY_PRICE: 29.95,
   PRO_MONTHLY_LINK: 'https://buy.stripe.com/9AQ00l29mfeE8Vy6ov',
-  PRO_YEARLY_PRICE: 299.95,
+  PRO_YEARLY_PRICE: 249.95,
   PRO_YEARLY_LINK: 'https://buy.stripe.com/6oE6oJcO01nOc7K3ci',
 }
